e5b92a7b25c15d41fa37c7979a91bd5bb72604e8
Nan27Hid <nhanbui1422hdn@gmail.com>
2023-08-08 15:34:58 +0700

    add to

---
+++ Const_and_Pointers.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+using namespace std;
+
+int main () {
+    int high_score {100};
+    int low_score {65};
+
+    //const int *score_ptr {&high_score};
+    //cout << *score_ptr << endl;
+    //*score_ptr = 86; // ERROR
+    //score_ptr = &low_score;
+    //cout << *score_ptr;
+
+    int *const score_ptr {&high_score};
+    cout << *score_ptr << endl;
+    *score_ptr = 86; // Ok
+    //score_ptr = &low_score; // Error
+    cout << *score_ptr;
+
+}
